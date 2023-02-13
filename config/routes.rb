491493213609c672da14168f487ab3db7a9a56f8
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-02-13 19:28:32 +0200

    changed root path

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
 
+  root to: "todos#index"
+
   resources :todos do
     resources :items
   end
